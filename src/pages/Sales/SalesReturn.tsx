6d21500074864408656e310cea6f8fc1ef852fae
MrArdi2416 <dev.dardi999@gmail.com>
2024-06-20 05:13:20 +0530

    first commit

--- src/pages/Sales/SalesReturn.tsx
+++ src/pages/Sales/SalesReturn.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 const SalesReturn = () => {
   return (
     <div>
-            <h1 className="text-3xl mb-4">Sales Return</h1>
-
+       <h1 className="text-3xl mb-4">Sales Return</h1>
     </div>
   )
 }
